3f903f7af7cc7f60c4fe3a365decca7c48898c21
Manjula <mguneratne3@gmail.com>
2024-01-19 16:43:08 -0500

    API base code has been set up

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,39 @@
+export default class KanbanAPI {
+    static getItem(columnId){
+        const column = read().find(column => column.id == columnId);
+
+        if (!column){
+            return [];
+        }
+
+        return column.items
+    }
+
+}
+
+function read(){
+    const json = localStorage.getItem("kanban-data");
+
+    if (!json){       //runs on default
+        return [
+            {
+                id: 1,
+                items: []
+            },
+            {
+                id: 2,
+                items: []
+            },
+            {
+                id: 3,
+                items: []
+            }
+        ];
+    }
+
+    return JSON.parse(json);
+}
+
+function save(data){
+    localStorage.setItem("kanban-data", JSON.stringify(data));
+}

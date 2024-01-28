ed9fb9f4a3b07500f187ffb6854066c7337f5236
Manjula Guneratne <mguneratne3@gmail.com>
2024-01-28 01:36:21 -0500

    Items + Blur added

--- js/view/Column.js
+++ js/view/Column.js
@@ -12,7 +12,9 @@ export default class Column{
         this.elements.title.textContent = title;
 
         this.elements.addItem.addEventListener("click", () => {
-            // TODO: add item
+            const newItem = KanbanAPI.insertItem(id, "");
+
+            this.renderItem(newItem);
         });
 
         KanbanAPI.getItems(id).forEach(item => {
@@ -32,5 +34,11 @@ export default class Column{
             <button class="kanban__add-item" type="button">+ Add</button>
         </div>
         `).children[0];
+    }
+
+    renderItem(data){
+        const item = new Item(data.id, data.content);
+
+        this.elements.items.appendChild(item.elements.root);
     }
 }
